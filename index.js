7a1309b5e16e68076e4c6f22068da6a101648a25
hoquangtri289 <hoquangtri289@gmail.com>
2020-06-29 22:48:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({message: "hello to dev"});
+    res.json({message: "hello to dev", new: "/new"});
     console.log('hello');
 })
 
