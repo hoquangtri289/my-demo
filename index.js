3c7b36439964f579b3bb99464988c5b262f5ad4e
hoquangtri289 <hoquangtri289@gmail.com>
2020-06-29 20:19:11 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({message: "hello to dev"});
+    console.log('hello');
+})
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server on run at port: ${port}`);
+})
